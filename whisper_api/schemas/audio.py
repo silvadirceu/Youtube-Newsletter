55c284bb94ca4988dc513775ed2cf728feb3aec5
Luiz Brandi <luiz.brandi@innovox.com.br>
2024-09-02 12:18:08 -0500

    adding Audio schema

---
+++ whisper_api/schemas/audio.py
@@ -0,0 +1,35 @@
+from fastapi import UploadFile
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class AudioBase(BaseModel):
+    file: UploadFile
+
+
+# Properties to receive via API on creation
+class AudioCreate(AudioBase):
+    pass
+
+
+# Properties to receive via Bussiness on creation
+class AudioBussinessCreate(AudioBase):
+    pass
+
+# Properties to receive via API on update
+class AudioUpdate(AudioBase):
+    pass
+
+class AudioInDBBase(AudioBase):
+    pass
+    class Config:
+        pass
+
+# Additional properties to return via API
+class Audio(AudioInDBBase):
+    pass
+
+
+class AudioInfo(Audio):
+    pass
